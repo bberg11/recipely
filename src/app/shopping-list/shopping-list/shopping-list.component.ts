0337c50a517e5f37af146e8220ee5b11d81c0272
Brian Berg <bberg11@gmail.com>
2020-04-22 23:33:34 -0400

    Add Ingredient model

--- src/app/shopping-list/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list/shopping-list.component.ts
@@ -1,15 +1,20 @@
+import { Ingredient } from './../../shared/ingredient.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.scss']
+  styleUrls: ['./shopping-list.component.scss'],
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Potatoes', '1 lb'),
+    new Ingredient('Steaks', '3'),
+    new Ingredient('Eggs', '2'),
+    new Ingredient('Whole Milk', '2 cups'),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
