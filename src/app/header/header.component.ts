0e8afca7d9281528374579f3ad56a45aaba4aaba
Brian Berg <bberg11@gmail.com>
2020-04-23 13:05:44 -0400

    Add basic navigation functionality

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  @Output() navLinkClicked = new EventEmitter<string>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onNavClick(event: Event, slug: string): void {
+    event.preventDefault();
+    this.navLinkClicked.emit(slug);
   }
-
 }
