ea301c390dab77682f54c3478cb20c9c8650f733
Brian Berg <bberg11@gmail.com>
2020-04-22 23:33:20 -0400

    Add Recipe model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,30 @@
+import { Recipe } from './../recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Recipe One',
+      'Description of Recipe One. Yadda yadda yadda',
+      `https://picsum.photos/id/${this.randomNumber()}/200`
+    ),
+    new Recipe(
+      'Recipe Two',
+      'Description of Recipe Two. Nom nom nom',
+      `https://picsum.photos/id/${this.randomNumber()}/200`
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  randomNumber(): number {
+    return Math.floor(Math.random() * 1000) + 1;
   }
-
 }
